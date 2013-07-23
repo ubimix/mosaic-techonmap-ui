7e09ef4735e7026eb218fa46320e7ee052579aa0
ubimix <sysop@ns383704.ovh.net>
2013-07-23 17:54:54 +0200

    - updated config - loaded config.js before the app scripts

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 
 
 window.appConfig =  {
-    'loadUrl': '',
-    'storeUrl': '',
+    'loadUrl': function() {return '/xwiki/bin/view/XWiki/ExportService?id=m3&xpage=plain';},
+    'storeUrl': function() {return '/xwiki/bin/view/XWiki/StoreService?id=m3&xpage=plain';},
     'loginCheckUrl': function() {return '/xwiki/bin/view/XWiki/UserIdentificationService?xpage=plain';},
     'authenticationUrl': function(id) {return '/xwiki/bin/login/XWiki/XWikiLogin?sl_provider=twitter&xredirect=/techonmap-dev/edition.html%23'+id;},
     'lastTweetUrl': function() { return '/xwiki/bin/view/m3/TwitterFeedReader?xpage=plain';}
