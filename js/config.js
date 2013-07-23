0922e776459bf2293e687c9a327c00ac2feae62d
ubimix <sysop@ns383704.ovh.net>
2013-07-23 22:25:58 +0200

    - updated config to techonmap.ubimix.com

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 
 
 window.appConfig =  {
-    'loadUrl': function() {return '/xwiki/bin/view/XWiki/ExportService?id=m3&xpage=plain';},
-    'storeUrl': function() {return '/xwiki/bin/view/XWiki/StoreService?id=m3&xpage=plain';},
-    'loginCheckUrl': function() {return '/xwiki/bin/view/XWiki/UserIdentificationService?xpage=plain';},
+    'loadUrl': function() {return '/xwiki/bin/view/ubimix/ExportService?id=wiki&xpage=plain';},
+    'storeUrl': function() {return '/xwiki/bin/view/ubimix/StoreService?id=wiki&xpage=plain';},
+    'loginCheckUrl': function() {return '/xwiki/bin/view/ubimix/UserIdentificationService?xpage=plain';},
     'authenticationUrl': function(id) {return '/xwiki/bin/login/XWiki/XWikiLogin?sl_provider=twitter&xredirect=/techonmap-dev/edition.html%23'+id;},
-    'lastTweetUrl': function() { return '/xwiki/bin/view/m3/TwitterFeedReader?xpage=plain';}
+    'lastTweetUrl': function() { return '/xwiki/bin/view/ubimix/TwitterFeedReader?xpage=plain';}
 
 }
